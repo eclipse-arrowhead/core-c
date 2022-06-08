355b0c1a8929c784204ee425f73cc01dda08e8a9
Emanuel Palm <emanuel.palm@pinterop.se>
2022-06-08 16:25:55 +0200

    Minor comment fix

--- libraries/ah_mbedtls/include/ah/mbedtls.h
+++ libraries/ah_mbedtls/include/ah/mbedtls.h
@@ -27,7 +27,7 @@ struct ah_mbedtls_server {
     AH_I_TLS_SERVER_FIELDS
 };
 
-// The provided SSL config must be set in client mode.
+// The provided SSL config must be set to client mode.
 ah_extern ah_err_t ah_mbedtls_client_init(ah_mbedtls_client_t* client, ah_tcp_trans_t trans, mbedtls_ssl_config* ssl_conf, ah_mbedtls_on_handshake_done_cb on_handshake_done_cb);
 ah_extern ah_tcp_trans_t ah_mbedtls_client_as_trans(ah_mbedtls_client_t* client);
 ah_extern ah_mbedtls_client_t* ah_mbedtls_client_get_from_conn(ah_tcp_conn_t* conn);
@@ -35,7 +35,7 @@ ah_extern int ah_mbedtls_client_get_last_err(ah_mbedtls_client_t* client);
 ah_extern int ah_mbedtls_client_get_last_err_from_conn(ah_tcp_conn_t* conn);
 ah_extern void ah_mbedtls_client_term(ah_mbedtls_client_t* client);
 
-// The provided SSL config must be set in server mode.
+// The provided SSL config must be set to server mode.
 ah_extern ah_err_t ah_mbedtls_server_init(ah_mbedtls_server_t* server, ah_tcp_trans_t trans, mbedtls_ssl_config* ssl_conf, ah_mbedtls_on_handshake_done_cb on_handshake_done_cb);
 ah_extern ah_mbedtls_server_t* ah_mbedtls_server_get_from_listener(ah_tcp_listener_t* ln);
 ah_extern int ah_mbedtls_server_get_last_err(ah_mbedtls_server_t* server);
